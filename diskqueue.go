7054d5453a5972c7ba53cd7f2274e831aeea5d41
Evan Lin <evanslin@gmail.com>
2016-02-18 14:11:52 +0800

    add some basic function

--- diskqueue.go
+++ diskqueue.go
@@ -1,6 +1,33 @@
 package diskqueue
 
+import (
+	"bufio"
+	"bytes"
+	"os"
+	"sync"
+)
+
 type Diskqueue struct {
+	//Inherite from sync RWMutex
+	sync.RWMutex
+
+	//File and Buffer operation
+	readFile  *os.File
+	writeFile *os.File
+	reader    *bufio.Reader
+	writeBuf  bytes.Buffer
+
+	//working channel
+	readChan chan []byte
+
+	writeChan         chan []byte
+	writeResponseChan chan error
+
+	emptyChan         chan int
+	emptyResponseChan chan error
+
+	exitChan     chan int
+	exitSyncChan chan int
 }
 
 //Interface of Diskqueue
@@ -20,15 +47,57 @@ func NewDiskqueue() WorkQueue {
 }
 
 // Put data to diskqueue
-func (d *Diskqueue) Put([]byte) error {
-	return nil
+func (d *Diskqueue) Put(data []byte) error {
+	d.RLock()
+	defer d.RUnlock()
+
+	d.writeChan <- data
+	return <-d.writeResponseChan
 }
 
 // Read data from disk queue
 // this is expected to be an *unbuffered* channel
 func (d *Diskqueue) ReadChan() []byte {
+	return <-d.readChan
+}
+
+func (d *Diskqueue) writeDataToFile(data []byte) error {
+	return nil
 }
 
 // Major working thread to handle concurrency
 func (d *Diskqueue) inLoop() {
+
+	//data for readChan
+	var data []byte
+	//To store total data write
+	var count int64
+	readC := new(chan []byte)
+
+	//TODO process data
+
+	for {
+
+		select {
+		case readC <- data:
+			// Handle read case
+			// the Go channel spec dictates that nil channel operations (read or write)
+			// in a select are skipped, we set r to d.readChan only when there is data to read
+
+		case <-d.emptyChan:
+			// Handle empty command thread
+		case dataWrite := <-d.writeChan:
+			// Handle write case
+			count++
+			d.writeResponseChan <- d.writeDataToFile(dataWrite)
+
+		case <-d.exitChan:
+			// Handle exist case
+			goto exit
+		}
+	}
+exit:
+
+	d.exitSyncChan <- 1
+
 }
